ae9154e266e59bad560bed5a137b5005112bc5a4
Riccardo Balin <riccardobalin@Riccardos-MacBook-Pro.local>
2025-06-14 22:28:21 -0600

    update plotting

---
+++ Dragon/plot/fine-tune/plot_train_history.py
@@ -0,0 +1,10 @@
+import numpy as np
+import glob
+import matplotlib
+from matplotlib import pyplot as plt
+font = {
+        'family' : 'serif',
+        'weight' : 'normal',
+        'size'   : 18}
+matplotlib.rc('font', **font)
+
